5b422d4c58954a754ec81ce0d73fb719541172d7
lshillman <lshillman@users.noreply.github.com>
2022-06-22 21:35:14 -0700

    add arrays of characters. Add functions for collecting length and uppercase prefs.

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specials = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+console.log(specials);
+
+function collectLength() { // prompts the user to enter a length, then checks to see if it's valid, returns the input if so, re-prompts if not
+  var passwordLength = prompt("How long should your password be? Please type a number between 8 and 128:");
+  console.log("length is " + passwordLength);
+  if ((passwordLength > 7) && (passwordLength < 129)) {
+    return passwordLength;
+  } else {
+    alert("Password must be at least 8 characters and no more than 128 characters. Please try again!");
+    collectLength();
+  }
+}
+
+function collectUpperPref() {
+  var upperPref = prompt("Include uppercase letters (A-Z)? Type y or n:").toUpperCase();
+  if ((upperPref == "Y") || (upperPref == "N")) {
+    console.log(upperPref);
+  } else {
+    alert('I need a "Y" or "N" here; please try again.');
+    collectUpperPref();
+  }
+}
+
+
+
+function generatePassword() {
+  collectLength();
+  collectUpperPref();
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
